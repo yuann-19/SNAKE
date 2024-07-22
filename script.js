672257cfe4f519d4aba9637358c8c0bf0e117891
yuann-19 <juan.ortiz.01805@maristascomayagua.edu.hn>
2024-07-21 23:38:02 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const lobi=new Audio("lobi.mp3");
+lobi.loop=true;
+lobi.play();
+const boton=document.getElementById('btn');
+boton.addEventListener("click",
+function(){
+window.location.href = 'https://codepen.io/Kevin1Pereira/pen/KKjMPGK';});
