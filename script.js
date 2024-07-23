1d4081c1bd3f63affa7ec307debd1810c1880b01
yuann-19 <juan.ortiz.01805@maristascomayagua.edu.hn>
2024-07-22 23:53:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const lob=new Audio("lobi.mp3");
+const click=new Audio("click.mp3");
 lob.loop=true;
 document.addEventListener('DOMContentLoaded', function() {
     lob.play();
@@ -6,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
 const boton=document.getElementById('btn');
 boton.addEventListener("click",
 function(){
+click.play();
 window.location.href = 'https://yuann-19.github.io/SNAKE-GAME/';});
 var miAudio = document.getElementById('miAudio');
 miAudio.play();
